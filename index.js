2e22b00c50b88cff368f26add47813ac58cb598f
Landon Crabtree <me@landon.pw>
2025-01-12 19:09:59 -0600

    Express refactor skeleton

--- index.js
+++ index.js
@@ -1,18 +1,97 @@
-const express = require('express');
-const cors = require('cors');
-// require("dotenv").config();
-const app = express();
-const port = 3000; // You can choose any port you like
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session')
+var MemoryStore = require('memorystore')(session)
+var process = require('process');
+var cors = require('cors');
+var crypto = require('crypto');
+var dotenv = require('dotenv');
 
+// Load environment variables
+dotenv.config();
 
-app.use("/", require("./methods/login.js"));
+// Helpers
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+// async function initialize() {
+//   try {
+//     await database.init();
+//   } catch (error) {
+//     // If there is an error initializing the services, log the error and exit the process
+//     console.error(error);
+//     process.exit(1);
+//   }
+// }
+
+var app = express();
+
+// Routes
+var authRouter = require('./routes/auth')
+
+// Initialize the database
+// initialize();
+
+// Enable CORS
+app.use(cors());
+
+// Don't need this since this is only a backend API
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// Enable sessions with memory store to prevent memory leaks
+sess = {
+  secret: process.env.SESSION_SECRET || crypto.randomBytes(20).toString('hex'),
+  resave: false,
+  saveUninitialized: false,
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  cookie: { 
+    maxAge: 30*24*60*60*1000, // 30 days
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Using production settings')
+  // app.set('trust proxy', 1) // trust nginx proxy
+} else {
+  console.log('Using development settings')
+}
+
+app.use(session(sess))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+  } else {
+    res.status(err.status || 500);
+    res.json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000`);
 });
+
+module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/test", async (req, res) => {
     res.status(200).json({"message": "All Good Here"})
 })
